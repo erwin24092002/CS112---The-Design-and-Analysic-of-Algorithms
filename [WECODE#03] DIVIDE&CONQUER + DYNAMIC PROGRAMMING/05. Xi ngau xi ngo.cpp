8b21a0e23e3c9a6fd721cc3dc682724e119694b4
Erwin2409 <83054910+erwin24092002@users.noreply.github.com>
2022-06-25 02:16:34 +0700

    Create 05. Xi ngau xi ngo.cpp

---
+++ [WECODE#03] DIVIDE&CONQUER + DYNAMIC PROGRAMMING/05. Xi ngau xi ngo.cpp
@@ -0,0 +1,32 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	ll s;
+	cin >> s;
+
+	vector<ll> dp(s + 1, 0);
+	dp[0] = 1;
+	for (ll sum = 1; sum <= s; sum++)
+	{
+		for (ll dice = 1; dice <= 6; dice++)
+		{
+			if (sum - dice >= 0)
+			{
+				dp[sum] += dp[sum - dice];
+				dp[sum] %= (ll(1e9 + 7));
+			}
+		}
+	}
+	cout << dp[s] << endl;
+
+	return 0;
+}
