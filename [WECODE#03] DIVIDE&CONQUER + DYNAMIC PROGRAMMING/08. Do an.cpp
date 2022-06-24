06c721a78cc7815f5b876100e9bd8f0facde64ed
Erwin2409 <83054910+erwin24092002@users.noreply.github.com>
2022-06-24 14:45:12 +0700

    Create 08. Do an.cpp

---
+++ [WECODE#03] DIVIDE&CONQUER + DYNAMIC PROGRAMMING/08. Do an.cpp
@@ -0,0 +1,53 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+
+struct subject
+{
+	ll start;
+	ll end;
+	ll credit;
+};
+
+/*
+* dp[i] là số tín chỉ tối đa có thể đạt được trong thời gian từ 1 -> i
+*
+* CÔNG THỨC QUY HOẠCH ĐỘNG:
+* dp[i] = max(dp[subjects[j].end] + subjects[i].credit, dp[subjects[i-1].end])
+*				j where subject[j].end < subject[i].start
+* dp[0] = 0
+*/
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	ll n;
+	cin >> n;
+	vector<subject> subjects(n);
+	for (ll i = 0; i < n; i++)
+		cin >> subjects[i].start >> subjects[i].end >> subjects[i].credit;
+	sort(subjects.begin(), subjects.end(),
+		[](subject a, subject b) {
+			return a.end < b.end;
+		});
+
+	map<ll, ll> dp;
+	dp[0] = 0;
+	ll ans = LLONG_MIN;
+	for (auto sj : subjects)
+	{
+		auto it = dp.lower_bound(sj.start);
+		it--;
+		ll total_credit = it->second + sj.credit;
+		ans = max(ans, total_credit);
+		dp[sj.end] = ans;
+	}
+	cout << ans << endl;
+
+	return 0;
+}
