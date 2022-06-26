c6bfcc53a7da15c7bc869f1d3378b2ad5f1ce656
Erwin2409 <83054910+erwin24092002@users.noreply.github.com>
2022-06-26 15:02:19 +0700

    Create 04. Toa nha cao tang.cpp

---
+++ [WECODE#03] DIVIDE&CONQUER + DYNAMIC PROGRAMMING/04. Toa nha cao tang.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+
+/*
+* BRUTE FORCE:
+*/
+
+struct buiding
+{
+	ll l, r, h;
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	ll n;
+	cin >> n;
+	vector<buiding> buidings(n);
+	for (auto& i : buidings)
+		cin >> i.l >> i.r >> i.h;
+
+	ll city_size = 1e6;
+	vector<ll> hist(city_size);
+	for (auto i : buidings)
+		for (ll j = i.l; j <= i.r; j++)
+			hist[j] = max(hist[j], i.h);
+
+	if (hist[0])
+		cout << 0 << " " << hist[0] << endl;
+	for (ll i = 0; i < city_size - 1; i++)
+	{
+		if (hist[i + 1] > hist[i])
+			cout << i + 1 << " " << hist[i + 1] << endl;
+		if (hist[i + 1] < hist[i])
+			cout << i << " " << hist[i + 1] << endl;
+	}
+
+	return 0;
+}
